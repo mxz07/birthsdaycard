6fb7637f4c63d6712a160a0fc5882193a0fcdc91
mxz07 <710690219@qq.com>
2022-03-24 19:56:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/two/NextActivity.java
@@ -0,0 +1,15 @@
+package com.example.two;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class NextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+
+}
